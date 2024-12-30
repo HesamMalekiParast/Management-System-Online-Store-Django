16e09cbd2381718e746f03d676c12de8e27784b5
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2024-12-30 14:14:48 +0330

    project updated!

--- src/settings.py
+++ src/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # other apps
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates', 'home/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
