8bd4adf88ea597b75a02e95aa79a201a97f4d044
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-01 19:34:24 +0330

    project updated!

--- home/admin.py
+++ home/admin.py
@@ -11,4 +11,3 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
-    search_fields = ('name', 'category.name')
