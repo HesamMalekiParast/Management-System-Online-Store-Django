18331e14eb2af0edda35efd609e162956cb839a4
HesamMalekiParast <125996052+HesamMalekiParast@users.noreply.github.com>
2025-01-01 22:19:39 +0330

    project updated!

--- home/views.py
+++ home/views.py
@@ -5,3 +5,14 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html',{'products': products})
+
+
+def product_list(request):
+    query = request.GET.get('q')  # Get the search query from the URL
+    if query:
+        # Filter products based on the search query
+        product_search = Product.objects.filter(name__icontains=query)
+    else:
+        product_search = []
+
+    return render(request, 'product_list.html', {'product_search': product_search})
